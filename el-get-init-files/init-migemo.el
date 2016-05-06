2ade8b27a709fff448b972cc339e2cdaa16c7fe5
smotokezuru <hisami.shimizu@gmail.com>
2016-05-07 00:21:53 +0900

    migemo起動設定を変更

--- el-get-init-files/init-migemo.el
+++ el-get-init-files/init-migemo.el
@@ -1,8 +1,12 @@
-(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict"
-      migemo-command "/usr/local/bin/cmigemo"
-      migemo-options '("-q" "--emacs")
-      migemo-user-dictionary nil
-      migemo-coding-system 'utf-8
-      migemo-regex-dictionary nil)
-(migemo-init)
-
+;;; Commentary:
+;;; migemo設定
+;;; Code:
+(when (require 'migemo nil t)
+  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict"
+        migemo-command "/usr/local/bin/cmigemo"
+        migemo-options '("-q" "--emacs")
+        migemo-user-dictionary nil
+        migemo-coding-system 'utf-8
+        migemo-regex-dictionary nil)
+  (migemo-init))
+;;; init-migemo ends here
